d58f98298fa54762b0ab3ad742b8cf624244df16
David Kotlirevsky <david.kotlirevsky@gmail.com>
2023-06-22 11:53:58 -0300

    driver gears first approach with ifs

--- driver-gears/src/solution.js
+++ driver-gears/src/solution.js
@@ -1,7 +1,40 @@
 
-function solution (position) {
 
+class Car {
+    static MAXIMUM_FIRST_GEAR = 30;
+    static MAXIMUM_SECOND_GEAR = 50;
+    static MAXIMUM_THIRD_GEAR = 80;
+    static MAXIMUM_FOURTH_GEAR = 110;
+    static ZERO_SPEED = 0;
 
 }
 
-module.exports = {solution}
+class Driver {
+    static FIRST_GEAR = "First gear";
+    static SECOND_GEAR= "Second gear";
+    static THIRD_GEAR= "Third gear";
+    static FOURTH_GEAR= "Fourth gear";
+
+    changeGear(gear) {
+        if (gear === Driver.FIRST_GEAR){
+            return Car.MAXIMUM_FIRST_GEAR;
+        }
+        if (gear === Driver.SECOND_GEAR){
+            return Car.MAXIMUM_SECOND_GEAR;
+        }
+        if (gear === Driver.THIRD_GEAR){
+            return Car.MAXIMUM_THIRD_GEAR;
+        }
+        if (gear === Driver.FOURTH_GEAR){
+            return Car.MAXIMUM_FOURTH_GEAR;
+        }
+        return Car.ZERO_SPEED
+    }
+}
+
+function solutionGears (gear) {
+    const driver = new Driver()
+    return driver.changeGear(gear)
+}
+
+module.exports = {solutionGears, Car, Driver}
--- driver-gears/test/solution.test.js
+++ driver-gears/test/solution.test.js
@@ -2,19 +2,23 @@
 const {
 	expect
 } = require('chai')
-const { solution }= require("../src/solution");
+const { solutionGears, Driver, Car }= require("../src/solution");
 
 
 describe('blank', () =>{
 	function doTest(sut, expected){
-		it(`given ${sut} then ${expected}`, () => expect(solution(sut)).to.equal(expected));
+		it(`given a driver which executes ${sut} then car get this ${expected} speed as maximum`, () => expect(solutionGears(sut)).to.equal(expected));
 	}
 
 	function doTestError(sut, expected){
 		it(`given ${sut} then fail with ${expected}`, () => expect(() => solution(sut[0],sut[1])).to.throw(expected));
 	}
 
-	doTest(0, 0);
+	doTest(Driver.FIRST_GEAR, Car.MAXIMUM_FIRST_GEAR);
+	doTest(Driver.SECOND_GEAR, Car.MAXIMUM_SECOND_GEAR);
+	doTest(Driver.THIRD_GEAR, Car.MAXIMUM_THIRD_GEAR);
+	doTest(Driver.FOURTH_GEAR, Car.MAXIMUM_FOURTH_GEAR);
+	doTest("other", Car.ZERO_SPEED);
 	// doTest(1, 1);
 	// doTest(2, 1);
 	// doTest(3, 2);
